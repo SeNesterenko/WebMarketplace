6517b3dd8e77971a3e89ecb195e6c6a59c8777b5
snesterenko <snesterenko@dtln.ru>
2022-09-08 19:38:27 +0300

    lost files

---
+++ WebMarketplace/Migrations/20220905162005_Test.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebMarketplace.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_UserId1",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UserId1",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UserId",
+                table: "Products",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_UserId",
+                table: "Products",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_UserId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UserId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UserId1",
+                table: "Products",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_UserId1",
+                table: "Products",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
